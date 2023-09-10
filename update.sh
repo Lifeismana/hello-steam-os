070fef77478eb0e38a41371fab9b8e6a21eef1f3
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2023-09-10 21:12:20 +0200

    more process

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ cd "${0%/*}"
 echo "Processing SteamOS..."
 
 ProcessDepot ".dll"
+ProcessDepot ".exe"
+ProcessDepot ".pyz"
 FixUCS2
 
 CreateCommit "$(cat bin/version.txt | grep -o '[0-9\.]*')" "$1"
