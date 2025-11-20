7ddebdb78bde935af358071663fdeb89d2a28f18
BOT <15911822+Lifeismana@users.noreply.github.com>
2025-11-20 19:19:55 +0000

    | 52 files | M ChangeLog,  M devkit-gui.sh,  M windows-client/ChangeLog,  M windows-client/devkit-gui.exe_strings.txt,  M windows-client/devkit-msvsmon/.gitignore,  M windows-client/devkit-msvsmon/msvsmoninstall.py,  M windows-client/devkit-utils/README.md,  M windows-client/devkit-utils/devkit_utils/__init__.py,  M windows-client/devkit-utils/devkit_utils/resolve.py,  M windows-client/devkit-utils/steam-client-create-shortcut,  M windows-client/devkit-utils/steam-devkit-rpc,  M windows-client/devkit-utils/steamos-delete,  M windows-client/devkit-utils/steamos-dump-controller-config,  M windows-client/devkit-utils/steamos-get-status,  M windows-client/devkit-utils/steamos-list-games,  M windows-client/devkit-utils/steamos-prepare-upload,  M windows-client/devkit-utils/steamos-set-steam-client,  M windows-client/frozen_application_license.txt,  M windows-client/python3_strings.txt,  M windows-client/third-party-licenses/Apache-2.0.txt,  M windows-client/third-party-licenses/Artistic.txt,  M windows-client/third-party-licen

--- devkit-gui.sh
+++ devkit-gui.sh
@@ -22,8 +22,13 @@ if [ -n "${STEAM_RUNTIME-}" ]; then
 fi
 
 # Find a suitable OS-level python interpreter
-for p in python3.11 python3.10 python3.9 python3 python; do
+for p in python3.12 python3.11 python3.10 python3.9 python3 python; do
   if [ -n "$(which $p 2>/dev/null)" ]; then
+    if $($p -c 'import sys; sys.exit(0) if sys.version_info.major == 3 and sys.version_info.minor == 12 else sys.exit(1)'); then
+      PYTHON=$p
+      PYZ=devkit-gui-cp312.pyz
+      break
+    fi
     if $($p -c 'import sys; sys.exit(0) if sys.version_info.major == 3 and sys.version_info.minor == 11 else sys.exit(1)'); then
       PYTHON=$p
       PYZ=devkit-gui-cp311.pyz
@@ -45,30 +50,37 @@ done
 # No suitable python found, check for a working pyenv
 if [ -z "${PYTHON-}" ]; then
   if [ -n "$(which pyenv 2>/dev/null)" ]; then
-    PYENV_VERSION="$(pyenv versions | grep -o '3\.11.*')"
+    PYENV_VERSION="$(pyenv versions | grep -o '3\.12.*')"
     if [ -n "${PYENV_VERSION-}" ]; then
       export PYENV_VERSION="$PYENV_VERSION"
-      PYTHON="pyenv exec python3.11"
-      PYZ=devkit-gui-cp311.pyz
+      PYTHON="pyenv exec python3.12"
+      PYZ=devkit-gui-cp312.pyz
     else
-      PYENV_VERSION="$(pyenv versions | grep -o '3\.10.*')"
+      PYENV_VERSION="$(pyenv versions | grep -o '3\.11.*')"
       if [ -n "${PYENV_VERSION-}" ]; then
         export PYENV_VERSION="$PYENV_VERSION"
-        PYTHON="pyenv exec python3.10"
-        PYZ=devkit-gui-cp310.pyz
+        PYTHON="pyenv exec python3.11"
+        PYZ=devkit-gui-cp311.pyz
       else
-        PYENV_VERSION="$(pyenv versions | grep -o '3\.9.*')"
+        PYENV_VERSION="$(pyenv versions | grep -o '3\.10.*')"
         if [ -n "${PYENV_VERSION-}" ]; then
           export PYENV_VERSION="$PYENV_VERSION"
-          PYTHON="pyenv exec python3.9"
-          PYZ=devkit-gui-cp39.pyz
+          PYTHON="pyenv exec python3.10"
+          PYZ=devkit-gui-cp310.pyz
+        else
+          PYENV_VERSION="$(pyenv versions | grep -o '3\.9.*')"
+          if [ -n "${PYENV_VERSION-}" ]; then
+            export PYENV_VERSION="$PYENV_VERSION"
+            PYTHON="pyenv exec python3.9"
+            PYZ=devkit-gui-cp39.pyz
+          fi
         fi
       fi
     fi
 
     if [ -z "${PYTHON-}" ]; then
-      pyenv versions | grep '^\s*3\.\(11\|10\|9\)'
-      log "pyenv installed but no python 3.11, 3.10 or 3.9 versions found"
+      pyenv versions | grep '^\s*3\.\(12\|11\|10\|9\)'
+      log "pyenv installed but no python 3.12, 3.11, 3.10 or 3.9 versions found"
       log "Run 'pyenv install <version>' with a version listed above"
       exit 1
     fi
@@ -77,7 +89,7 @@ fi
 
 if [ -z "${PYTHON-}" ]; then
   log "No usable python found"
-  log "Please install python 3.11, 3.10 or 3.9 from your package manager or via pyenv"
+  log "Please install python 3.12, 3.11, 3.10 or 3.9 from your package manager or via pyenv"
   log "e.g apt install python3.11"
   log "    pacman -S pyenv"
   exit
